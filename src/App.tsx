15e94900a309bf1f56ec8364f80d4f300e54c136
IgorSouza4489 <lokaiextreme@gmail.com>
2025-09-05 17:17:17 -0300

    zelma change log

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/*remove*/}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
